e88384373b2acc399d9844ec896ba2e287bbf83a
sj3-2 <nuanox.school@gmail.com>
2025-05-21 18:03:37 +0900

    [종합] 수행평가 4개 추가.

---
+++ scripts/assignments/데이터과학/[6.11] 데이터과학 코랩 그래프.js
@@ -0,0 +1,5 @@
+AssignmentRegistry.register({
+    title: "데과 - 코랩 수행평가",
+    description: "6월 11일 데이터과학 수행평가 준비하기.\n지금까지 배운 코랩 코드 복습하기.\n\n[ 수행평가 정보 ]\n- 평가일: 6월 11일(수요일)",
+    dueDate: "2025-06-11"
+});
